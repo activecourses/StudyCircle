e39803037eab6ec3d1dae340b93c55f8feb6afea
amrmostafa800 <amrmostafa800@gmail.com>
2024-10-12 20:08:41 +0300

    Fix: Use Serilog ASP.NET Core Instead of Serilog without it, remove unused returns

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -3,6 +3,12 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog((context, services, configuration) =>
+{
+    configuration
+        .ReadFrom.Configuration(context.Configuration);  // Reads the Serilog config from appsettings.json
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -13,10 +19,6 @@ builder.Services.AddControllers();
 builder.Services.AddBusinessService();
 
 var app = builder.Build();
-
-using var log = new LoggerConfiguration()
-    .WriteTo.File("Logs/log.txt")
-    .CreateLogger();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- backend/Database/Database.cs
+++ backend/Database/Database.cs
@@ -1,16 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace Database
 {
     public static class Database
     {
-        public static IServiceCollection AddDatabaseService(this IServiceCollection services)
+        public static void AddDatabaseService(this IServiceCollection services)
         {
             // Add the DbContext
             services.AddDbContext<AppDbContext>();
-
-            return services;
         }
     }
 }
