3bb39a481f00db92076f5be0db59c1a6f4216923
Mohamed Adel <99229298+mohamed-adel-alsayed@users.noreply.github.com>
2024-10-22 13:00:31 +0300

    Added filters and Jwt

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -1,5 +1,11 @@
+using API;
+using API.Filters;
 using Business;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +17,39 @@ builder.Services.AddCors();
 builder.Services.AddControllers();
 
 builder.Services.AddBusinessService();
+
+
+
+// Bearer Token Authentication 
+var jwtOptions = builder.Configuration.GetSection("Jwt").Get<JwtOptions>();
+builder.Services.AddSingleton(jwtOptions);
+builder.Services.AddAuthentication().
+    AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+    {
+        // to keep the token string after getting the info 
+        // then it can be accessed using HttpContext
+        options.SaveToken = true;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,  
+            ValidIssuer = jwtOptions.Issuer,
+
+            ValidateAudience = true,  
+            ValidAudience = jwtOptions.Audience,
+             
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SigningKey))
+        };
+    });
+
+//
+builder.Services.AddScoped<ClubMemberFilter>();
+builder.Services.AddScoped<ClubOwnerFilter>();
+builder.Services.AddScoped<ClubModeratorFilter>();
+
+
 
 var app = builder.Build();
 
